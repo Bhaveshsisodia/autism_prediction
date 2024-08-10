b72fede8bc50e8417bea6ddb4f2586a3cadf5f43
Bhavesh Kumar Lohar <bhaveshsisodia2@gmail.com>
2024-08-10 15:46:15 +0530

    model_trainer_pipeline created

--- Autism/entity/artifact_entity.py
+++ Autism/entity/artifact_entity.py
@@ -14,3 +14,35 @@ class DataValidationArtifact:
     invalid_train_file_path: str
     invalid_test_file_path: str
     drift_report_file_path: str
+
+@dataclass
+class DataTransformationArtifact:
+    transformed_object_file_path: str
+    transformed_train_file_path: str
+    transformed_test_file_path: str
+
+
+# @dataclass
+# class ClassificationMetricArtifact:
+#     f1_score: float
+#     precision_score: float
+#     recall_score: float
+
+@dataclass
+class ModelTrainerArtifact:
+    is_trained: bool
+    message: str
+    trained_model_file_path : str
+    train_acc: float
+    test_acc : float
+    train_prec: float
+    test_prec: float
+    train_recall: float
+    test_recall : float
+    model_accuracy: float
+
+
+
+
+
+
