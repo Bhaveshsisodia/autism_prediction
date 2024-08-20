9aff7e52b5d1ee292e2ed0a0344ef288d48f44bc
Bhavesh Kumar Lohar <bhaveshsisodia2@gmail.com>
2024-08-20 12:34:14 +0530

    'update'

--- autism_django/autism_django/settings.py
+++ autism_django/autism_django/settings.py
@@ -15,7 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -25,7 +24,7 @@ SECRET_KEY = "django-insecure-ydib3mu@wjb=(5s&go%cz!bb!l)889v#ayp8p^nolcat3)6z1u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".vercel.app",".now.sh"]
+ALLOWED_HOSTS =[] #[".vercel.app",".now.sh"]
 
 
 # Application definition
@@ -119,9 +118,15 @@ USE_TZ = True
 
 import os
 
-STATIC_URL = "/static/"
+# STATIC_URL = "static/"
+# STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
+
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
