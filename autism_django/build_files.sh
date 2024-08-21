b330b802ec40b53ceae674fda2bfd6224d73a599
Bhavesh Kumar Lohar <bhaveshsisodia2@gmail.com>
2024-08-21 16:28:49 +0530

    'updated'

--- autism_django/build_files.sh
+++ autism_django/build_files.sh
@@ -4,6 +4,6 @@ set -e
 
 python3.12 -m ensurepip --upgrade
 python3.12 -m pip install --upgrade pip
-python3.12 -m pip install requirements.txt
+python3.12 -m pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput --clear
 echo "BUILD END"
