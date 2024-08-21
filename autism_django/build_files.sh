5ebbd8f3a410d9315f05d4e887b66ecc632d1231
Bhavesh Kumar Lohar <bhaveshsisodia2@gmail.com>
2024-08-21 16:34:13 +0530

    'updated'

--- autism_django/build_files.sh
+++ autism_django/build_files.sh
@@ -1,8 +1,9 @@
 
 echo "BUILD START"
+set -e
 
-python3.12 -m ensurepip --upgrade
-python3.12 -m pip install --upgrade pip
-python3.12 -m pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput --clear
+python3.9 -m ensurepip --upgrade
+python3.9 -m pip install --upgrade pip
+pip install --no-cache-dir -r /vercel/path0/autism_django/requirements.txt
+python3.9 manage.py collectstatic --noinput --clear
 echo "BUILD END"
