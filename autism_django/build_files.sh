fad705015935126c39582d7a4052fcc20180b226
Bhavesh Kumar Lohar <bhaveshsisodia2@gmail.com>
2024-08-21 15:21:36 +0530

    'updated'

--- autism_django/build_files.sh
+++ autism_django/build_files.sh
@@ -1,8 +1,6 @@
 
 echo "BUILD START"
 set -e
-pyenv install 3.9.19
-pyenv global 3.9.19
 python3.9 -m ensurepip --upgrade
 python3.9 -m pip install --upgrade pip
 pip install --no-cache-dir -r /vercel/path0/autism_django/requirements.txt
