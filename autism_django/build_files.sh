d5c06b5c6aa38ec740e125aed8ef5ab026977076
Bhavesh Kumar Lohar <70841901+Bhaveshsisodia@users.noreply.github.com>
2025-07-03 21:00:05 +0530

    Delete autism_django/build_files.sh

--- autism_django/build_files.sh
+++
@@ -1,8 +0,0 @@
-
-echo "BUILD START"
-set -e
-python3.9 -m ensurepip --upgrade
-python3.9 -m pip install --upgrade pip
-python3.9 -m pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END"
